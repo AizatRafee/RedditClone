1a4edf4fd32633d5a8efbcdc8b4764b23bb1b477
aizat <aizat.rafee@gmail.com>
2020-02-10 12:25:01 +0800

    [aizat](aizat-dev)add comment

--- app/src/main/java/com/example/redditclone/util/BindingAdapter.kt
+++ app/src/main/java/com/example/redditclone/util/BindingAdapter.kt
@@ -7,6 +7,7 @@ import androidx.databinding.BindingAdapter
  * Created by aizat on 10/2/2020
  */
 
+// if more than 1000, display format like 12.1K
 @BindingAdapter("voteCount")
 fun setVoteCount(textView: TextView, count: Int) {
     textView.text = if (count >= 1000 || count <= -1000) {
